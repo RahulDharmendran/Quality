3ee262cafd245d932b036f1c289d53c8236d8c49
RahulDharmendran <rahuldharmendran1912@gmail.com>
2025-12-19 13:20:10 +0530

    feat: Add a new formatter for converting OData V2 Edm.Time objects and ISO 8601 duration strings to HH:MM:SS format.

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -3,45 +3,63 @@ sap.ui.define([], function () {
 
     return {
         formatTime: function (sTime) {
-            if (!sTime) {
+            if (sTime === null || sTime === undefined) {
                 return "";
             }
-            try {
-                // Parse PT05H41M16S format
-                var sHours = "00";
-                var sMinutes = "00";
-                var sSeconds = "00";
 
-                // Remove PT
-                var sCleanTime = sTime.replace("PT", "");
+            // Handler for OData V2 Edm.Time object (which has an 'ms' property)
+            if (typeof sTime === "object" && sTime.ms !== undefined) {
+                var fSeconds = sTime.ms / 1000;
+                var iH = Math.floor(fSeconds / 3600);
+                var iM = Math.floor((fSeconds % 3600) / 60);
+                var iS = Math.floor(fSeconds % 60);
 
-                var iH = sCleanTime.indexOf("H");
-                var iM = sCleanTime.indexOf("M");
-                var iS = sCleanTime.indexOf("S");
+                var sHours = iH.toString();
+                var sMinutes = iM.toString();
+                var sSeconds = iS.toString();
 
-                if (iH > -1) {
-                    sHours = sCleanTime.substring(0, iH);
-                }
-
-                var iMStart = (iH > -1) ? (iH + 1) : 0;
-                if (iM > -1) {
-                    sMinutes = sCleanTime.substring(iMStart, iM);
-                }
-
-                var iSStart = (iM > -1) ? (iM + 1) : ((iH > -1) ? (iH + 1) : 0);
-                if (iS > -1) {
-                    sSeconds = sCleanTime.substring(iSStart, iS);
-                }
-
-                // Simple padding without ES2017 padStart for compatibility
                 if (sHours.length < 2) sHours = "0" + sHours;
                 if (sMinutes.length < 2) sMinutes = "0" + sMinutes;
                 if (sSeconds.length < 2) sSeconds = "0" + sSeconds;
 
                 return sHours + ":" + sMinutes + ":" + sSeconds;
-            } catch (e) {
-                return sTime;
             }
+
+            // Handler for String format like "PT05H41M16S"
+            if (typeof sTime === "string") {
+                try {
+                    var sCleanTime = sTime.replace("PT", "");
+                    var iH = sCleanTime.indexOf("H");
+                    var iM = sCleanTime.indexOf("M");
+                    var iS = sCleanTime.indexOf("S");
+
+                    var sH = "00";
+                    var sMin = "00";
+                    var sSec = "00";
+
+                    if (iH > -1) {
+                        sH = sCleanTime.substring(0, iH);
+                    }
+                    var iMStart = (iH > -1) ? (iH + 1) : 0;
+                    if (iM > -1) {
+                        sMin = sCleanTime.substring(iMStart, iM);
+                    }
+                    var iSStart = (iM > -1) ? (iM + 1) : ((iH > -1) ? (iH + 1) : 0);
+                    if (iS > -1) {
+                        sSec = sCleanTime.substring(iSStart, iS);
+                    }
+
+                    if (sH.length < 2) sH = "0" + sH;
+                    if (sMin.length < 2) sMin = "0" + sMin;
+                    if (sSec.length < 2) sSec = "0" + sSec;
+
+                    return sH + ":" + sMin + ":" + sSec;
+                } catch (e) {
+                    return sTime;
+                }
+            }
+
+            return "";
         }
     };
 });
